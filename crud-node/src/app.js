8dce692d0e750f26b8e52c0833dbe49d93434f5f
Vinayak  Hulyal <113448017+vins9526@users.noreply.github.com>
2025-04-21 21:54:48 +0530

    edit env file

--- crud-node/src/app.js
+++ crud-node/src/app.js
@@ -4,13 +4,13 @@ const cors = require("cors");
 const userRoutes = require("./routes/userRoute");
 const app = express();
 const PORT = process.env.PORT || 5000;
-const apiUrl = process.env.REACT_APP_API_URL;
+const apiUrl = process.env.NODE_API;
 
 
 app.use(bodyParser.json());
 app.use(cors());
 app.listen(PORT ,() => {
-    console.log(`Server is running on ${apiUrl}l:${PORT}`)
+    console.log(`Server is running on ${apiUrl}:${PORT}`)
 })
 
 app.use("/api/user" , userRoutes);
