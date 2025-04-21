10d12cca454f1121830c1fd3da3dd30f0bc2e6c6
Vinayak  Hulyal <113448017+vins9526@users.noreply.github.com>
2025-04-22 01:12:11 +0530

    adding env files

--- crud-node/src/app.js
+++ crud-node/src/app.js
@@ -5,16 +5,12 @@ const cors = require("cors");
 const userRoutes = require("./routes/userRoute");
 const app = express();
 const PORT = process.env.PORT || 5000;
-const nodeApi = process.env.NODE_API;
-
-
-console.log("NODE_API:", nodeApi); // Debugging
-console.log("PORT:", PORT); // Debugging
+const host = process.env.HOST;
 
 app.use(bodyParser.json());
 app.use(cors());
 app.listen(PORT ,() => {
-    console.log(`Server is running on ${nodeApi}:${PORT}`)
+    console.log(`Server is running on ${host}:${PORT}`)
 })
 
 app.use("/api/user" , userRoutes);
