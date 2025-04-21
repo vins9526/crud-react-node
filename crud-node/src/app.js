1a7402f07a95d65b3f59d0dc874c81ca2c29ac47
Vinayak  Hulyal <113448017+vins9526@users.noreply.github.com>
2025-04-21 23:22:32 +0530

    edited files

--- crud-node/src/app.js
+++ crud-node/src/app.js
@@ -1,16 +1,20 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const userRoutes = require("./routes/userRoute");
 const app = express();
 const PORT = process.env.PORT || 5000;
-const apiUrl = process.env.NODE_API;
+const nodeApi = process.env.NODE_API;
 
+
+console.log("NODE_API:", nodeApi); // Debugging
+console.log("PORT:", PORT); // Debugging
 
 app.use(bodyParser.json());
 app.use(cors());
 app.listen(PORT ,() => {
-    console.log(`Server is running on ${apiUrl}:${PORT}`)
+    console.log(`Server is running on ${nodeApi}:${PORT}`)
 })
 
 app.use("/api/user" , userRoutes);
