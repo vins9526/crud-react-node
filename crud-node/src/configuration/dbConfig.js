fec2356b4d54301c5ba78b8c2fe0ad9d1e240ca4
Vinayak  Hulyal <113448017+vins9526@users.noreply.github.com>
2025-04-25 16:06:19 +0530

    db connection issue

--- crud-node/src/configuration/dbConfig.js
+++ crud-node/src/configuration/dbConfig.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://vinayakhulyal6143:d0Wp6nhBDO1wPvRW@crud_db.rrd01xh.mongodb.net",{
+mongoose.connect("mongodb+srv://vinshulyal:jqMKqApajZYbI7Nj@crudb.f8yvmgx.mongodb.net/",{
 })
 
 mongoose.connection.on("connected",()=>{
